84cf8ec775777a8d0ac677d343411cd163d9ab37
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-09-29 12:06:43 -0300

    feat: implementação do front-end com autenticação via Google

--- barberease/barberease/urls.py
+++ barberease/barberease/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pessoa.urls")),
+    path('accounts/', include('allauth.urls')),
+    path('auth/', include('allauth.socialaccount.urls')),
 ]
--- barberease/pessoa/models.py
+++ barberease/pessoa/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from allauth.account.signals import user_logged_in
+from allauth.socialaccount.models import SocialAccount
+from django.shortcuts import redirect
 
 # class Empresa(models.Model):
 #     pass
@@ -10,9 +15,21 @@ from django.contrib.auth.models import User
 class Cliente(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField("Nome", max_length=150)
-    sobrenome = models.CharField("Sobrenome", max_length=150)
+    sobrenome = models.CharField("Sobrenome", max_length=150, null=True, blank=True)
     email = models.EmailField("E-mail", unique=True, null=True, blank=True)
 
 
     def __str__(self):
         return self.user | self.user
+    
+@receiver(post_save, sender=User)
+def signal_create_cliente(sender, instance, created, **kwargs):
+    if created:
+        Cliente.objects.create(user=instance, email=instance.email, nome=instance.first_name, sobrenome=instance.last_name)
+
+@receiver(user_logged_in)
+def custom_user_logged_in(sender, request, user, **kwargs):
+    social_account = SocialAccount.objects.filter(user=user, provider='google').first()
+
+    if social_account is None:
+        return redirect('pessoa:cadastro')
